bf1951f8426637ac58741921cba6d53a42d2d057
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-09-07 16:00:15 -0300

    Aula 4

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Pernambuco",
+    descricao:
+      "As comunidades quilombolas de Pernambuco representam um importante capítulo da história brasileira. Em Pernambuco, diversas comunidades quilombolas estão localizadas, principalmente no interior do estado, contribuindo para a diversidade cultural e histórica da região.",
+    link: "https://g1.globo.com/pe/pernambuco/noticia/2023/07/27/censo-2022-pernambuco-e-o-quinto-estado-do-brasil-com-maior-populacao-quilombola-com-78827-pessoas.ghtml",
+  },
+  {
+    titulo: "Bahia",
+    descricao:
+      "A Bahia é o estado brasileiro com a maior população quilombola. As comunidades quilombolas baianas possuem uma rica história e cultura, contribuindo significativamente para a identidade do estado.",
+    link: "https://g1.globo.com/ba/bahia/noticia/2023/07/27/bahia-tem-maior-no-de-quilombolas-no-pais-5-das-10-cidades-de-maiores-populacoes-quilombolas-ficam-no-estado-aponta-censo-do-ibge.ghtml",
+  },
+  {
+    titulo: "Maranhão",
+    descricao:
+      "O Maranhão também possui uma forte presença de comunidades quilombolas. Essas comunidades preservam tradições e costumes ancestrais, enriquecendo o patrimônio cultural maranhense.",
+    link: "https://pt.wikipedia.org/wiki/Comunidades_quilombolas_no_Maranh%C3%A3o",
+  },
+];
